f91644a67ecdc65830ab1bc8037f7c5e741ce41b
Abdul Basit Khatri <129309925+AbdulBasitKhatri@users.noreply.github.com>
2024-11-18 20:38:03 -0800

    Initial upload

---
+++ script.js
@@ -0,0 +1,192 @@
+const wordList = ["cattle","hot","file","ripe","bee","play","boast","keen","pies","loving","gabby","soft","trap","better","shirt","vast","fat","divide","bouncy","range","puffy","shiny","jar","simple","force","nifty","six","man","gifted","rhythm","shade","sheep","high","summer","jail","gaping","wacky","flame","allow","doctor","erect","fork","right","guard","daffy","nest","arrive","pick","silent","moon","wall","crook","grape","wax","robust","rain","scold","copper","mark","brash","brainy","stale","fuzzy","game","corn","closed","poised","bore","shut","first","time","fold","type","yak","string","cats","double","degree","store","ratty","reason","sponge","new","beef","bent","haunt","crawl","pail","close","dock","waves","brave","act","late","legal","full","ticket","rule","smooth","fear","straw","super","writer","somber","expand","card","tan","quilt","owe","clap","grin","talk","squeal","shape","yam","cough","blood","charge","fluffy","well","nice","cure","rock","sedate","stove","admit","tumble","water","dime","bag","cheer","tank","tick","pizzas","chop","extra","large","dusty","fixed","meek","zoom","warm","delay","try","square","frog","purple","glove","solid","zipper","red","zippy","green","desert","lumpy","lip","sad","alive","plain","sticks","leg","finger","light","glue","jolly","letter","spiky","wail","wise","dogs","plug","pump","good","motion","crib","price","trashy","pinch","cold","skirt","bawdy","word","fix","faint","ocean","houses","horses","left","toy","tame","versed","strong","unit","living","beam","quill","obtain","scrape","plough","teeth","bit","matter","can","guide","signal","snotty","cactus","list","move","refuse","messy","flash","old","attend","true","hole","shrill","sturdy","drawer","stop","meal","pricey","dare","well","to","do","is","butter","juggle","class","trick","loose","push","lick","murky","lethal","hungry","ahead","top","shy","well","off","punish","curve","weight","hand","skate","yoke","son","gentle","excite","punch","homely","basket","pause","school","sticky","rapid","power","person","blue","eyed","ink","desire","warn","smash","rub","near","anger","irate","house","blind","groan","puny","temper","chance","blow","remind","stroke","mere","party","pretty","chubby","plot","breezy","celery","sweet","relax","baby","steel","sour","broken","verse","growth","swift","maid","bolt","queen","cow","live","sun","tempt","office","call","prefer","cloth","shave","blade","awake","itchy","chin","plucky","giant","adhoc","pan","foot","glass","bushes","sugar","want","snatch","hook","share","way","gratis","burly","upset","tacit","shaggy","bare","drain","retire","animal","mint","avoid","groovy","fuel","far","flung","flow","side","faulty","paltry","spade","fall","clammy","fog","turn","toe","aloof","pull","absent","bump","turkey","look","rough","thin","copy","rush","rare","elbow","ruin","bead","trees","women","judge","madly","earthy","limit","fierce","boring","bury","stew","remain","wild","sable","design","flock","fail","yell","love","salty","trip","earth","plane","toad","amuck","oval","uneven","step","hammer","comb","filthy","desk","theory","testy","best","seal","black","bruise","work","long","sound","amount","ship","form","roomy","shame","tacky","route","half","creepy","decide","abaft","rail","icy","nation","boil","spicy","tight","branch","sordid","daily","thread","poor","dreary","bumpy","join","ray","safe","absurd","line","heavy","stiff","memory","vanish","kiss","acidic","cannon","door","vessel","battle","load","knotty","sack","obey","ball","empty","shoes","ritzy","pat","second","border","brick","sky","rings","enjoy","argue","drunk","pedal","flag","harm","ground","coil","things","please","rate","ill","swanky","pest","debt","elfin","elite","pour","mate","north","drink","lowly","phone","zesty","income","kind","loss","add","grade","prick","plate","snakes","soup","tough","cars","racial","flesh","army","inform","mature","pop","silly","day","view","loud","deer","glib","wary","upbeat","cagey","stone","report","spoon","cut","lonely","hop","known","deeply","base","sloppy","cross","arch","locket","badge","birth","fry","goofy","tie","soothe","x","ray","lucky","sink","detail","home","royal","bucket","ghost","chalk","slimy","future","dry","tip","proud","common","steep","icky","gaze","offend","ill","fated","belong","dinner","scale","crown","torpid","tidy","babies","minor","gusty","agree","rat","flood","tour","change","trucks","three","knife","paste","fresh","place","dizzy","knot","detect","spray","flavor","exist","invent","crush","melted","actor","tawdry","cave","pushy","tested","fire","glossy","sneeze","page","nerve","farm","crayon","level","huge","screw","train","board","sassy","snail","pear","offer","grate","switch","fish","cable","caring","repeat","rural","mean","kitty","arrest","boat","obese","horse","fast","answer","cry","dam","rainy","gun","male","peck","grease","attach","unruly","buzz","sin","wet","breath","steer","slim","tall","recess","meaty","ruddy","pie","seat","return","size","open","post","brown","noisy","heal","camp","care","money","bath","muscle","chess","cake","bathe","yellow","trite","strap","cows","woozy","value","far","bright","crowd","hunt","tramp","crime","cherry","grass","thumb","twist","edge","save","behave","handle","men","potato","even","horn","enter","health","zip","borrow","wealth","rot","ignore","reward","muddle","air","next","supply","five","lazy","hushed","wood","sip","alike","toys","search","snake","rifle","dance","year","taste","tent","back","chunky","quaint","market","narrow","tricky","ducks","count","secret","nasty","trust","juice","camera","plants","cub","one","rake","cycle","road","expect","river","regret","spare","foamy","hurt","club","sore","sneaky","real","savory","pack","cooing","wrong","shaky","boy","unlock","peep","pot","legs","lamp","canvas","vase","feeble","giddy","slow","scare","meddle","joyous","rice","cause","hat","rhyme","clever","egg","war","books","weigh","lame","spoil","wave","wind","jewel","fool","bake","curly","macho","humor","kill","itch","placid","metal","bow","moldy","remove","stay","lean","idea","bait","whole","mitten","brake","nutty","brass","rose","advice","spill","pale","hug","kick","drag","pray","sense","rude","blink","toes","aware","uppity","sign","wreck","ear","tiger","march","gather","walk","scarce","spring","smart","touch","false","giants","land","vest","free","dirty","burst","coach","awful","number","pipe","press","wing","windy","linen","modern","trail","cakes","raspy","lace","guess","happen","ugly","flashy","dull","old","orange","wheel","skin","guitar","ill","dress","clover","peel","tree","snore","equal","film","kneel","fine","knee","hose","harbor","spark","stuff","basin","float","weak","hover","stem","crate","stupid","risk","watery","many","effect","sock","polish","bang","two","extra","small","soak","tart","hair","normal","record","uncle","famous","middle","miss","fly","jam","useful","week","shock","past","wide","nose","tangy","powder","thaw","bubble","ants","jumpy","busy","lovely","laugh","mug","seed","yawn","carry","unused","used","coat","soda","craven","dark","rest","large","town","ring","space","scent","blot","frail","sail","noise","stingy","help","bite","sort","cobweb","fade","strip","roof","test","cellar","trade","cover","boot","extend","null","dad","admire","blush","bear","yummy","smoggy","rustic","quince","calm","wine","four","gaudy","action","wrap","cook","eight","teeny","tiny","zonked","suck","moan","mouth","advise","voyage","hall","skip","nut","jog","steam","visit","reach","bird","twig","trot","hard","to","find","crazy","frogs","reply","title","shoe","sulky","classy","heap","fax","wound","icicle","girl","peace","ban","pine","pet","happy","milk","scary","sick","sigh","round","shiver","mend","soap","skinny","rigid","flower","knock","quirky","clip","vague","snow","sleet","drown","tasty","quiver","flimsy","accept","decay","grubby","cute","tap","error","tiny","able","root","tin","grain","sheet","swim","heady","eye","scream","tooth","sand","slave","pen","drip","need","silk","wooden","wipe","jelly","slope","month","engine","taboo","system","beg","box","eggnog","crow","tub","shop","weary","bounce","table","story","pocket","dirt","fact","tease","deep","robin","cat","colour","bleach","label","gamy","young","credit","brawny","vein","clear","repair","tired","lying","poke","abrupt","circle","angle","mind","wink","waste","alert","hum","color","disarm","drop","end","nimble","white","zany","well","made","button","throat","rely","window","raise","thrill","reject","public","amuse","curvy","polite","found","cap","prose","acrid","stick","sister","pickle","cent","wiry","worry","flight","dog","poison","hour","object","bottle","own","book","bless","harsh","pink","chief","mixed","sprout","dust","fill","field","group","head","intend","oven","bat","cruel","sniff","veil","curl","fence","ten","mask","paper","suffer","kettle","fowl","thank","mellow","saw","superb","stream","jaded","cloudy","magic","star","manage","tongue","damp","loaf","whine","fair","small","order","bikes","cuddly","wren","bone","nippy","song","mist","porter","planes","little","bomb","use","voice","clean","print","name","bed","shelf","monkey","cast","grey","match","odd","angry","kindly","mice","quack","funny","unable","spy","shrug","belief","scrub","vulgar","soggy","milky","attack","aback","shake","like","car","meat","mix","ajar","slip","grip","doubt","ablaze","arm","eager","big","paddle","bite","sized","third","coal","church","hill","hands","annoy","launch","night","check","fancy","gate","abject","follow","mourn","crack","hope","key","lake","quartz","silver","sofa","serve","profit","girls","second","hand","rob","travel","escape","wrist","preach","invite","geese","lavish","juicy","stir","drum","claim","excuse","race","smell","curved","parcel","scarf","reign","phobic","wonder","zebra","start","fang","chase","bike","tax","mute","queue","chilly","whirl","wicked","point","naive","jazzy","front","high","cup","violet","tickle","trains","hate","appear","crabby","exotic","run","unpack","last","trace","wide","eyed","tender","aboard","jump","sleepy","harass","berry","handy","needle","gray","wry","rescue","tail","melt","part","marked","stage","mushy","spiffy","mine","hard","seemly","short","team","heat","swing","tow","jeans","lively","alarm","duck","witty","overt","early","stamp","scared","bored","dream","sea","pets","tense","rabbit","lunch","squash","elated","truck","brush","misty","fetch","spell","nine","note","afford","yard","rinse","lock","lumber","cheat","mom","teeny","rabid","birds","dear","cream","long","term","face","wobble","mighty","bells","floor","aunt","slap","hurry","dead","suit","rod","zinc","sharp","park","art","bell","blue","whip","snails","reduce","fit","salt","wait","choke","neat","bulb","rich","cool","furry","throne","same","permit","rotten","dolls","godly","tire","clumsy","steady","dapper","treat","sudden","cheese","black","and","white","chew","watch","tray","neck","expert","pumped","smoke","wish","room","spooky","unite","plant","kaput","murder","ice","death","wash","listen","coast","frame","ready","ski","nod","measly","worm","acid","donkey","quiet","thing","damage","flaky","stitch","nosy","quick","squeak","joke","ask","messup","eyes","innate","friend","show","sleep","needy","smelly","event","self","food","iron","stare","wiggly","female","winter","low","depend","oil","zephyr","street","drab","hollow","stain","wander","hang","fruit","cheap","songs","wrench","doll","burn","husky","cart","employ","great","few","unique","riddle","glow","insect","thick","van","wool","flap","spot","stormy","spotty","gold","plan","bloody","bitter","minute","utter","marble","scorch","nail","occur","inject","notice","rebel","scene","broad","nappy","lie","grab","amused","liquid","carve","pin","oafish","flat","island","pencil","ultra","lush","afraid","mass","mother","untidy","roll","settle","honey","crash","injure","bridge","knit","yarn","silky","moor","lewd","petite","beds","clam","jagged","wire","woman","earn","greet","zoo","bad","eggs","grumpy","brief","collar","smile","easy","paint","learn","fasten","marry","greasy","faded","tug","greedy","ragged","pass","apple","banana","grape","peach","lemon","mango","berry","melon","cherry","kiwi","plum","dates","fig","stone","cabbage","carrot","celery","radish","onion","garlic","beet","pumpkin","lettuce","pasta","sauce","stew","broth","meat","fish","chicken","taco","bowl","plate","fork","knife","spoon","drink","juice","soda","milk","water","wine","beer","rum","whiskey","punch","leaf","bark","trunk","root","thorn","bloom","petal","fern","moss","lily","rose","tulip","daisy","sun","moon","star","cloud","storm","rain","wind","beach","ocean","river","lake","pond","hill","mount","valley","plain","cave","stone","rock","sand","dirt","soil","grass","root","twig","bush","forest","field","flame","smoke","ash","earth","metal","wood","glass","clay","fiber","leaf","jump","run","swim","dance","sing","read","write","paint","build","drive","throw","catch","watch","climb","bake","clean","cook","play","laugh","cry","help","call","vice","start","dance","play","talk","ask","tell","jump","walk","happy","sad","quick","slow","bright","dark","blue","green","red","pink","great","small","large","young","old","clean","dirty","smooth","rough","sweet","bitter","salty","sour","crisp","fresh","ripe","tasty","spicy","bumpy","cloudy","quickly","slowly","softly","loudly","brightly","darkly","happily","sadly","eagerly","calmly","freely","nearly","totally","mostly","barely","really","suddenly","simply","and","but","or","for","nor","so","yet","if","because","although","while","since","unless","until","where","when","about","above","after","along","among","around","at","before","behind","below","beside","between","by","for","from","in","inside","near","of","on","over","through","to","under","until","with","without","against","across"];
+const wordsDiv = document.getElementById("words");
+const caret = document.getElementById("caret");
+const speed = document.getElementById("speed");
+const characters = document.getElementById("characters");
+const timeLeft = document.getElementById("timeLeft");
+const accuracy = document.getElementById("accuracy");
+const infoText = document.getElementById("infoText");
+const resultsDiv = document.getElementById("results");
+
+resultsDiv.style.display = "none";
+let mistakes = 0;
+let words = "";
+let charTyped = 0;
+let charIndex = 0;
+let basePos = null;
+let startTime = null;
+let duration = 30;
+let isStop = true;
+
+function generateWords(n){
+  for(let i=0; i<n; i++){
+    words += wordList[Math.floor(Math.random() * wordList.length)]+(i==n-1 ? "" : " ");
+  }
+}
+
+function styleWord(word){
+  let html = "";
+  for(let letter of word){
+    html += `<span class='`+(letter == " " ? "space" : "word")+`'>${letter}</span>`;
+  }
+  return html;
+}
+
+function type(e){
+  if(e.key == "Backspace"){
+    if(charIndex > 0){
+      charIndex--;
+      let l = wordsDiv.children[charIndex];
+      if(l.classList.contains("space")){
+        l.classList.remove("correct");
+        if(l.classList.contains("wrong")){
+          l.classList.remove("wrong");
+          mistakes--;
+        }
+      }
+       else{
+        l.classList.replace("correct","word");
+        if(l.classList.contains("wrong")){
+          l.classList.replace("wrong","word");
+          mistakes--;
+        }
+      }
+    }
+  }
+  else{
+    e.preventDefault();
+    e.stopPropagation();
+    if(e.key == words[charIndex]){
+      wordsDiv.children[charIndex].classList.add("correct");
+    }
+    else{
+      wordsDiv.children[charIndex].classList.add("wrong");
+      mistakes+=2;
+    }
+    if(charIndex == words.length-1){stop();return;}
+    else{
+      charIndex++;
+      if(charIndex > charTyped){
+        charTyped++;
+      }
+    }
+  }
+  setPos();
+}
+
+function getPos() {
+  const element = wordsDiv.children[charIndex];
+  if (!element) {
+    return null;
+  }
+  element.scrollIntoView({block: "center"});
+  const rect = element.getBoundingClientRect();
+  const x = rect.left + window.scrollX;
+  const y = rect.top + window.scrollY;
+  return {x,y};
+}
+
+function setPos(setBasePos = false){
+  let pos = getPos();
+  if(setBasePos){
+    basePos = pos || {x:0,y:0};
+  }
+  if(pos){
+    caret.style.color = (charIndex - 1 > -1 && wordsDiv.children[charIndex-1].classList.contains("wrong")) ? "tomato" : "#00ffae";
+    caret.style.left = pos.x-8+"px";
+    caret.style.top = pos.y-4+"px";
+  }
+}
+
+function currentTime(){
+  return Math.floor(Date.now()/1000);
+}
+
+function start(flag=true){
+  if(flag){
+    isStop = false;
+  }
+  resultsDiv.style.display = "none";
+  mistakes = 0;
+  words = "";
+  charTyped = 0;
+  charIndex = 0;
+  basePos = null;
+  startTime = null;
+  generateWords(40);
+  wordsDiv.innerHTML = styleWord(words);
+  wordsDiv.scrollTo(0,0);
+  setPos(true);
+  startTime = currentTime();
+}
+
+start(false);
+
+function calcSpeed(){
+  let _speed = `${Math.ceil( (charIndex/5) / ((currentTime()-startTime)/60) ) || 0}`;
+  if(_speed == "Infinity"){return "100 WPM";}
+  return `${_speed} WPM`;
+}
+
+function calcAccuracy(){
+  if(charIndex <= 0){}
+  let a = Math.round(((charTyped-mistakes)/charTyped)*100);
+  if(!a){a = 0;}
+  if(a < 0){a = 0;}
+  return `Accuracy: ${a}%`;
+}
+
+setInterval(function(){
+  if(isStop){speed.innerText = "0 WPM";timeLeft.innerText = `${duration}s`;return;}
+  speed.innerText = calcSpeed();
+  timeLeft_ = duration - (currentTime() - startTime);
+  if(timeLeft_ <= 0){stop();}
+  else{
+    timeLeft.innerText = `${timeLeft_}s`;
+  }
+},200);
+
+function stop(){
+  isStop = true;
+  resultsDiv.style.display = "block";
+  document.getElementById("speedT").innerText = calcSpeed().split(" ")[0];
+  document.getElementById("charactersT").innerText = charIndex;
+  document.getElementById("accuracyT").innerText = calcAccuracy().split(" ")[1].replace("%","");
+  document.getElementById("timeT").innerText = `${currentTime() - startTime} out of ${duration}`;
+  infoText.innerText = "Start typing OR Press (Esc) key to change";
+}
+
+window.addEventListener('keydown',function(e){
+  if(e.key.length > 1){
+    switch(e.key){
+      case "Backspace":
+        break;
+      case "Escape":
+        if(isStop){start(false);}
+        else{stop();}
+        return;
+        break;
+      default:
+        return;
+    }
+  }
+  if(isStop){
+    if(resultsDiv.style.display == "none"){
+      startTime = currentTime();
+      isStop=false;
+      infoText.innerText = "Press (Esc) key to end";
+    }
+    else{return;}
+  }
+  type(e);
+});
+
+wordsDiv.addEventListener('wheel',function(e){
+  e.preventDefault();
+});
+
+//TODO:
+/*
+caret and word font must be same everytime and font size of caret +1px than word
+recalculate basePos by saving the old scroll and then reset to 0 and restore it
+*/
